4805a14dd45abb1762af088a698e31b0b2930ad2
Ben Robbins <ben.robbins@gmail.com>
2016-09-24 10:04:28 -0700

    exclude cabin

--- rf.py
+++ rf.py
@@ -16,27 +16,27 @@ trainOutcomes = trainData['Survived']
 #TODO cabin could have meaning if it can predict how close they were to life boats? prolly not enough data
 trainData['numRelatives'] = trainData['SibSp'] + trainData['Parch']
 trainData['hasRelatives'] = trainData['numRelatives'].apply(lambda x: 1 if x > 0 else 0)
-trainData = trainData[trainData.columns.difference(['Survived', 'Name', 'Ticket', 'PassengerId', 'numRelatives', 'SibSp', 'Parch'])]
+trainData = trainData[trainData.columns.difference(['Survived', 'Name', 'Ticket', 'PassengerId', 'numRelatives', 'SibSp', 'Parch', 'Cabin'])]
 trainData['Sex'] = trainData['Sex'].apply(lambda x: 1 if x == 'male' else 0)
 avgAge = numpy.nanmean(trainData['Age'])
 trainData['Age'] = trainData['Age'].apply(lambda x: avgAge if math.isnan(x) else x)
 avgFare = numpy.nanmean(trainData['Fare'])
 trainData['Fare'] = trainData['Fare'].apply(lambda x: avgFare if math.isnan(x) else x)
 trainData['Embarked'] = trainData['Embarked'].apply(transformEmbarked)
-trainData['Cabin'] = trainData['Cabin'].apply(lambda x: 1 if isinstance(x, str) else 0)
+#trainData['Cabin'] = trainData['Cabin'].apply(lambda x: 1 if isinstance(x, str) else 0)
 
 testData = pandas.read_csv('./test.csv')
 passengerIds = testData['PassengerId']
 testData['numRelatives'] = testData['SibSp'] + testData['Parch']
 testData['hasRelatives'] = testData['numRelatives'].apply(lambda x: 1 if x > 0 else 0)
-testData = testData[testData.columns.difference(['Name', 'Ticket', 'PassengerId', 'numRelatives', 'SibSp', 'Parch'])]
+testData = testData[testData.columns.difference(['Name', 'Ticket', 'PassengerId', 'numRelatives', 'SibSp', 'Parch', 'Cabin'])]
 testData['Sex'] = testData['Sex'].apply(lambda x: 1 if x == 'male' else 0)
 testData['Age'] = testData['Age'].apply(lambda x: avgAge if math.isnan(x) else x)
 testData['Fare'] = testData['Fare'].apply(lambda x: avgFare if math.isnan(x) else x)
 testData['Embarked'] = testData['Embarked'].apply(transformEmbarked)
-testData['Cabin'] = testData['Cabin'].apply(lambda x: 1 if isinstance(x, str) else 0)
+#testData['Cabin'] = testData['Cabin'].apply(lambda x: 1 if isinstance(x, str) else 0)
 
-model = RandomForestClassifier(max_depth=8)
+model = RandomForestClassifier(max_depth=6)
 model.fit(trainData, trainOutcomes)
 
 prediction = model.predict(testData)
