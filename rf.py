2033c8efec44a8ed86e3c2e2d741efd628ac55c5
Ben Robbins <ben.robbins@gmail.com>
2016-09-24 10:00:40 -0700

    include hasRelatives

--- rf.py
+++ rf.py
@@ -14,7 +14,9 @@ trainOutcomes = trainData['Survived']
 #TODO decide whether there's anything important in the names
 #TODO decide whether to combine/partition children/sibling counts (could combine into a relatives count, could create yes/no for > N relatives, ...)
 #TODO cabin could have meaning if it can predict how close they were to life boats? prolly not enough data
-trainData = trainData[trainData.columns.difference(['Survived', 'Name', 'Ticket', 'PassengerId'])]
+trainData['numRelatives'] = trainData['SibSp'] + trainData['Parch']
+trainData['hasRelatives'] = trainData['numRelatives'].apply(lambda x: 1 if x > 0 else 0)
+trainData = trainData[trainData.columns.difference(['Survived', 'Name', 'Ticket', 'PassengerId', 'numRelatives', 'SibSp', 'Parch'])]
 trainData['Sex'] = trainData['Sex'].apply(lambda x: 1 if x == 'male' else 0)
 avgAge = numpy.nanmean(trainData['Age'])
 trainData['Age'] = trainData['Age'].apply(lambda x: avgAge if math.isnan(x) else x)
@@ -25,14 +27,16 @@ trainData['Cabin'] = trainData['Cabin'].apply(lambda x: 1 if isinstance(x, str) 
 
 testData = pandas.read_csv('./test.csv')
 passengerIds = testData['PassengerId']
-testData = testData[testData.columns.difference(['Name', 'Ticket', 'PassengerId'])]
+testData['numRelatives'] = testData['SibSp'] + testData['Parch']
+testData['hasRelatives'] = testData['numRelatives'].apply(lambda x: 1 if x > 0 else 0)
+testData = testData[testData.columns.difference(['Name', 'Ticket', 'PassengerId', 'numRelatives', 'SibSp', 'Parch'])]
 testData['Sex'] = testData['Sex'].apply(lambda x: 1 if x == 'male' else 0)
 testData['Age'] = testData['Age'].apply(lambda x: avgAge if math.isnan(x) else x)
 testData['Fare'] = testData['Fare'].apply(lambda x: avgFare if math.isnan(x) else x)
 testData['Embarked'] = testData['Embarked'].apply(transformEmbarked)
 testData['Cabin'] = testData['Cabin'].apply(lambda x: 1 if isinstance(x, str) else 0)
 
-model = RandomForestClassifier(max_depth=6)
+model = RandomForestClassifier(max_depth=8)
 model.fit(trainData, trainOutcomes)
 
 prediction = model.predict(testData)
