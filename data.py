5866d8f51e66a47567d15fd5176051bc6a2194ea
Ben Robbins <ben.robbins@gmail.com>
2016-09-24 13:28:47 -0700

    fill missing values using normal distribution

--- data.py
+++ data.py
@@ -4,11 +4,6 @@ import pandas
 import numpy
 import math
 
-def transformEmbarked(x):
-  if x == 'C': return 1
-  elif x == 'Q': return 2
-  else: return 3
-
 def getTrainData():
   trainData = pandas.read_csv('./train.csv')
   trainOutcomes = trainData['Survived']
@@ -22,39 +17,47 @@ def getTestDataKaggle():
 
 def dataLocal():
   trainData, trainOutcomes = getTrainData()
-  trainData, avgAge, avgFare = transformData(trainData)
+  trainData, avgAge, stdAge, avgFare, stdFare = transformData(trainData)
   trainData, testData, trainOutcomes, testOutcomes = train_test_split(trainData, trainOutcomes, test_size=0.15)
   return trainData, trainOutcomes, testData, testOutcomes
 
 def dataKaggle():
   trainData, trainOutcomes = getTrainData()
-  trainData, avgAge, avgFare = transformData(trainData)
+  trainData, avgAge, stdAge, avgFare, stdFare = transformData(trainData)
   testData, passengerIds = getTestDataKaggle()
-  testData, avgAge, avgFare = transformData(testData, avgAge, avgFare)
+  testData, avgAge, avgFare = transformData(testData, avgAge, stdAge, avgFare, stdFare)
   return trainData, trainOutcomes, testData, passengerIds
 
-def transformData(data, avgAge = None, avgFare = None):
+def transformData(data, avgAge = None, stdAge = None, avgFare = None, stdFare = None):
   data['numRelatives'] = data['SibSp'] + data['Parch']
   data['hasRelatives'] = data['numRelatives'].apply(lambda x: 1 if x > 0 else 0)
+  data['Sex'] = data['Sex'].apply(lambda x: 1 if x == 'male' else 0)
+
   if avgAge == None:
     avgAge = numpy.nanmean(data['Age'])
-  #TODO random?
-  data['Age'] = data['Age'].apply(lambda x: avgAge if math.isnan(x) else x)
-  data['isChild'] = data['Age'].apply(lambda x: 1 if x < 7 else 0)
-  data['Sex'] = data['Sex'].apply(lambda x: 1 if x == 'male' else 0)
+  if stdAge == None:
+    stdAge = numpy.nanstd(data['Age'])
+  data['Age'] = data['Age'].apply(lambda x: numpy.random.normal(avgAge, stdAge) if math.isnan(x) else x)
+  data['isChild'] = data['Age'].apply(lambda x: 1 if x < 10 else 0)
+
   if avgFare == None:
     avgFare = numpy.nanmean(data['Fare'])
-  #TODO random
-  data['Fare'] = data['Fare'].apply(lambda x: avgFare if math.isnan(x) else x)
+  if stdFare == None:
+    stdFare = numpy.nanstd(data['Fare'])
+  data['Fare'] = data['Fare'].apply(lambda x: numpy.random.normal(avgFare, stdFare) if math.isnan(x) else x)
+  data['hasHighFare'] = data['Fare'].apply(lambda x: 1 if x > avgFare else 0)
+
   embarked = pandas.get_dummies(data['Embarked'])
   embarked.drop(['Q'], axis=1, inplace=True)
   data = data.join(embarked)
+
   classes = pandas.get_dummies(data['Pclass'])
-  classes.columns = ['Class1', 'Class2', 'Class3']
-  classes.drop(['Class2'], axis=1, inplace=True)
+  classes.columns = ['isFirstClass', 'isSecondClass', 'isThirdClass']
+  classes.drop(['isSecondClass'], axis=1, inplace=True)
   data = data.join(classes)
-  data.drop(['Name', 'Ticket', 'PassengerId', 'numRelatives', 'SibSp', 'Parch', 'Cabin', 'Age', 'Embarked', 'Pclass'], axis=1, inplace=True)
-  return data, avgAge, avgFare
+
+  data.drop(['Name', 'Ticket', 'PassengerId', 'numRelatives', 'SibSp', 'Parch', 'Cabin', 'Age', 'Embarked', 'Pclass', 'Fare'], axis=1, inplace=True)
+  return data, avgAge, stdAge, avgFare, stdFare
 
 def testLocal(model):
   trainData, trainOutcomes, testData, testOutcomes = dataLocal()
@@ -77,5 +80,5 @@ def testKaggle(model):
 if __name__ == '__main__':
   from sklearn.ensemble import RandomForestClassifier
   from sklearn.svm import SVC
-  testKaggle(RandomForestClassifier(n_estimators=100, n_jobs=2, max_depth=6))
+  testLocal(RandomForestClassifier(n_estimators=100, n_jobs=2, max_depth=6))
   #testLocal(SVC(C=500, gamma=0.001))
